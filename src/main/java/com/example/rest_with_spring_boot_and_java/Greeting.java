bd1f9db10ec24ccb5d1c685c39c441a0d58399d8
CarlosBotelho42 <carlospiece55@gmail.com>
2024-09-03 21:30:20 -0300

    git.ignore tunado

---
+++ src/main/java/com/example/rest_with_spring_boot_and_java/Greeting.java
@@ -0,0 +1,20 @@
+package com.example.rest_with_spring_boot_and_java;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
